083fc04003b9717838f153e53b72bf88d1edb00f
Chenxueqing00 <930900787@qq.com>
2021-04-15 00:37:09 +0800

    fork 下来 改变之后提交

---
+++ src/com/zhiyou/dao/UserDao.java
@@ -0,0 +1,25 @@
+package com.zhiyou.dao;
+
+import com.zhiyou.model.User;
+
+/**
+ * @Author:灵魂码仔
+ * @Description:
+ * @Time: created on 2021/1/22 16:38
+ * @Email: 873227070@qq.com
+ */
+public class UserDao {
+
+    public User findUserByUsernameAndPassword(){
+        System.out.println("克隆项目 提交");
+        System.out.println("克隆项目 提交");
+        System.out.println("克隆项目 提交");
+        System.out.println("克隆项目 提交");
+        System.out.println("克隆项目 提交");
+        System.out.println("克隆项目 提交");
+        System.out.println("克隆项目 提交");
+        System.out.println("克隆项目 提交");
+        System.out.println("克隆项目 提交");
+        return null;
+    }
+}
